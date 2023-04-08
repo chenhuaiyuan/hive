d8d7f90e7cd50143348847f29f0d512c45a8a074
chenhuaiyuan <343938938@qq.com>
2023-04-09 00:06:06 +0800

    增加luau example

---
+++ example/luau/exception/base_exception.lua
@@ -0,0 +1,21 @@
+-- local response = require 'response'
+local response = hive.response
+
+-- 对错误的处理
+local function base_exception(code: number, message: string, status: number?)
+  local data = { code = code, message = message }
+  local resp = response.new():headers({
+    ['Content-type'] = 'application/json'
+  })
+  if status ~= nil then
+    resp = resp:status(status)
+  else
+    resp = resp:status(200)
+  end
+  return resp:body(data)
+  -- local resp = '<div>code: ' .. code .. '</div>'
+  -- resp = resp .. '<div>message: ' .. message .. '</div>'
+  -- return response.html(resp)
+end
+
+return base_exception
